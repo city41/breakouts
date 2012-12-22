f33a5e0ab7377a6f0037391b12a0c1d9bff34985
Matt Greer <matt.e.greer@gmail.com>
2012-12-21 21:08:21 -0700

    shortened impact version to fit on phone better

--- breakouts/impactjs/lib/game/main.js
+++ breakouts/impactjs/lib/game/main.js
@@ -1,19 +1,19 @@
-ig.module('game.main')
-.requires('game.games.menu', 'game.games.play')
-.defines(function() {
+ig.module('game.main').requires('game.games.menu', 'game.games.play').defines(function() {
 	ig.resetGame = function() {
 		ig.system.setGame(MenuGame);
 	};
-	
-	window.onload = function() {
-		window.scrollTo(0, 1);
-	};
 
-	if(ig.ua.mobile) {
-    ig.Sound.enabled = false;
-	}
+	window.addEventListener("load", function() {
+		setTimeout(function() {
+			window.scrollTo(0, 1);
 
-	ig.main('#canvas', MenuGame, 60, 320, 480, 1);
+		}, 0);
+
+		if (ig.ua.mobile) {
+			ig.Sound.enabled = false;
+		}
+
+		ig.main('#canvas', MenuGame, 60, 320, 416, 1);
+	});
 });
-
 
