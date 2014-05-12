5bcc92d4b1f64c9513c0289bfb3bff7a9601cb99
Phil Wickliffe <phillip.wickliffe@gmail.com>
2014-05-12 14:58:19 +1200

    Kiwi.js initial commit

---
+++ breakouts/kiwijs/src/States/GameComplete.js
@@ -0,0 +1,24 @@
+var GameCompleteState = new Kiwi.State('GameCompleteState');
+
+GameCompleteState.create = function () {
+    console.log('fuckity')
+    //bg
+    this.bg = new Kiwi.GameObjects.Sprite(this, this.textures.bg);
+    this.addChild(this.bg);
+
+    this.logo = new Kiwi.GameObjects.Sprite(this, this.textures.logo, 100, 100);
+    this.addChild(this.logo);
+
+    this.endTF = new Kiwi.HUD.Widget.TextField(this.game, 'Congratulations!', 120, 390);
+    this.endTF.style.color = "#000000";
+    this.endTF.style.fontSize = "18px";
+    this.endTF.style.fontFamily = "Arial, Helvetica, sans-serif"
+    this.game.huds.defaultHUD.addWidget(this.endTF);
+
+    this.game.input.onUp.add(this.clickGame, this);
+}
+
+GameCompleteState.clickGame = function () {
+    this.game.huds.defaultHUD.removeWidget(this.endTF);
+    game.states.switchState('IntroState');
+}
