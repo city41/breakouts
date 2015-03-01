cda82a50f3f2bcf8889b51a1be7abd680c0de166
Matt Greer <matt.e.greer@gmail.com>
2015-03-01 15:15:24 -0700

    impact: removed levels 3 and 4

--- breakouts/impactjs/lib/game/games/level-setups.js
+++ breakouts/impactjs/lib/game/games/level-setups.js
@@ -7,7 +7,6 @@ ig.module('game.games.level-setups').defines(function() {
 
 	ig.LevelSetups = [
 		{
-			name: "letsa begin",
 			bricks: [
 				[X,X,g,o,g,X,X],
 				[o,b,g,g,g,b,o],
@@ -15,7 +14,6 @@ ig.module('game.games.level-setups').defines(function() {
 			]
 		},
 		{
-			name: "how's it going?",
 			bricks: [
 				[X,g,o,g,o,g,X],
 				[X,b,b,b,b,b,X],
@@ -23,30 +21,6 @@ ig.module('game.games.level-setups').defines(function() {
 				[g,b,b,b,b,b,g],
 				[g,b,X,X,X,b,g],
 				[X,b,b,b,b,b,X]
-			]
-		},
-		{
-			name: 'tie fighta!',
-			bricks: [
-				[X,b,X,g,X,b,X],
-				[b,X,b,o,b,X,b],
-				[b,g,b,o,b,g,b],
-				[b,X,b,o,b,X,b],
-				[X,b,X,X,X,b,X],
-				[r,X,r,X,r,X,r]
-			]
-		},
-		{
-			name: 'swirl',
-			bricks: [
-				[r,g,o,b,r,g,o],
-				[b,X,X,X,X,X,X],
-				[o,X,o,b,r,g,o],
-				[g,X,g,X,X,X,b],
-				[r,X,r,X,r,X,r],
-				[b,X,b,o,g,X,g],
-				[o,X,X,X,X,X,o],
-				[g,r,b,o,g,r,b]
 			]
 		}
 	];
